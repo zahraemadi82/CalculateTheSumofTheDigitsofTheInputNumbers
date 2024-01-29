fd6bec4945f01d6feada22abf9303b2482f19f33
User <User@DESKTOP-1364VBG>
2024-01-29 17:16:59 +0330

    Add project files.

---
+++ CalculateTheSumofTheDigitsofTheInputNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter Number : ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+if(number.ToString().Length > 4)
+{
+    Console.WriteLine("Invalid input");
+}
+else
+{
+    for(int i = 0; i < 4; i++)
+    {
+        string n = number.ToString()[i].ToString();
+        sum += Convert.ToInt32(n);
+    }
+}
+
+Console.WriteLine(sum);
